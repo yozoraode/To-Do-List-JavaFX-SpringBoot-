5b03d79190066efc635e2d69af310e1442b178eb
yozoraode <salimdefrant11@gmail.com>
2026-01-25 13:49:14 +0700

    Initial commit

---
+++ src/main/java/com/defranto/VanillaToDo/VanillaToDoApplication.java
@@ -0,0 +1,13 @@
+package com.defranto.VanillaToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanillaToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanillaToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/defranto/VanillaToDo/VanillaToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defranto.VanillaToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanillaToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
